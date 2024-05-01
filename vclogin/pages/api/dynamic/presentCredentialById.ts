8e38b37d9b11ef6f7300d1af2501682daacb49a6
Ilayda Cansin Koc <ilaydacansin@gmail.com>
2024-05-01 23:00:47 +0000

    separete get and post method
    Signed-off-by: Ilayda Cansin Koc <ilaydacansin@gmail.com>

--- vclogin/pages/api/dynamic/presentCredentialById.ts
+++ vclogin/pages/api/dynamic/presentCredentialById.ts
@@ -17,123 +17,134 @@ try {
   console.error("Failed to connect to Redis:", error);
 }
 
+const getHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  console.log("LOGIN API GET BY ID");
+
+  // Get login_id from query
+  const uuid = req.query["login_id"];
+
+  // fetch policy from redis using uuid
+  const policy = await redis.get(uuid + "_policy");
+
+  // fetch inputDescriptor from redis using uuid
+  const inputDescriptor = await redis.get(uuid + "_inputDescriptor");
+  console.log("inputDescriptor: ", JSON.parse(inputDescriptor!));
+
+  //if policy is found
+  if (policy) {
+    const policyObject = JSON.parse(policy) as LoginPolicy;
+
+    // generate presentation definition using policy
+    // and inputDescriptor if it exists
+    const presentation_definition = generatePresentationDefinition(
+      policyObject,
+      inputDescriptor ? JSON.parse(inputDescriptor) : undefined,
+    );
+
+    const challenge = req.query["login_id"];
+
+    if (challenge) {
+      const token = await getToken(
+        challenge as string,
+        process.env.EXTERNAL_URL + "/api/dynamic/clientMetadataById",
+        process.env.EXTERNAL_URL + "/api/dynamic/presentCredentialById",
+        presentation_definition,
+        res,
+      );
+
+      res
+        .status(200)
+        .appendHeader("Content-Type", "application/oauth-authz-req+jwt")
+        .send(token);
+    }
+  } else {
+    res.status(500).end();
+    return;
+  }
+};
+
+const postHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  console.log("LOGIN API POST BY ID");
+
+  // Parse the JSON string into a JavaScript object
+  const presentation = JSON.parse(req.body.vp_token);
+  console.log("Presentation: \n", req.body.vp_token);
+
+  const uuid = presentation["proof"]["challenge"];
+  const policy = await redis.get(uuid + "_policy");
+
+  if (policy) {
+    const policyObject = JSON.parse(policy) as LoginPolicy;
+
+    // Constants for Redis to store the authentication result
+    const MAX_AGE = 20 * 60;
+    const EXPIRY_MS = "EX";
+
+    // Verify the presentation and the status of the credential
+    if (await verifyAuthenticationPresentation(presentation)) {
+      console.log("Presentation valid");
+      // Evaluate if the VP should be trusted
+      if (isTrustedPresentation(presentation, policyObject)) {
+        console.log("Presentation verified");
+
+        // Get the user claims when the presentation is trusted
+        const userClaims = extractClaims(presentation, policyObject);
+        console.log(userClaims);
+
+        // Store the authentication result in Redis
+        await redis.set(uuid + "_auth-res", "success", EXPIRY_MS, MAX_AGE);
+
+        // Store the user claims in Redis
+        await redis.set(
+          uuid + "_claims",
+          JSON.stringify(userClaims.tokenId),
+          EXPIRY_MS,
+          MAX_AGE,
+        );
+      } else {
+        console.log("Presentation not trusted");
+
+        await redis.set(
+          "auth_res:" + uuid,
+          "error_presentation_not_trused",
+          EXPIRY_MS,
+          MAX_AGE,
+        );
+        // Wallet gets an error message
+        res.status(500).end();
+        return;
+      }
+    } else {
+      console.log("Presentation invalid");
+      await redis.set(
+        "auth_res:" + uuid,
+        "error_invalid_presentation",
+        EXPIRY_MS,
+        MAX_AGE,
+      );
+      res.status(500).end();
+      return;
+    }
+
+    // Wallet gets 200 status code
+    res.status(200).end();
+  }
+};
+
+const handlers: any = {
+  POST: postHandler,
+  GET: getHandler,
+};
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>, //todo look for separate handles
 ) {
   try {
     const { method } = req;
-
-    if (method === "GET") {
-      console.log("LOGIN API GET BY ID");
-
-      // Get login_id from query
-      const uuid = req.query["login_id"];
-
-      // fetch policy from redis using uuid
-      const policy = await redis.get(uuid + "_policy");
-
-      // fetch inputDescriptor from redis using uuid
-      const inputDescriptor = await redis.get(uuid + "_inputDescriptor");
-      console.log("inputDescriptor: ", JSON.parse(inputDescriptor!));
-
-      //if policy is found
-      if (policy) {
-        const policyObject = JSON.parse(policy) as LoginPolicy;
-
-        // generate presentation definition using policy
-        // and inputDescriptor if it exists
-        const presentation_definition = generatePresentationDefinition(
-          policyObject,
-          inputDescriptor ? JSON.parse(inputDescriptor) : undefined,
-        );
-
-        const challenge = req.query["login_id"];
-
-        if (challenge) {
-          const token = await getToken(
-            challenge as string,
-            process.env.EXTERNAL_URL + "/api/dynamic/clientMetadataById",
-            process.env.EXTERNAL_URL + "/api/dynamic/presentCredentialById",
-            presentation_definition,
-            res,
-          );
-
-          res
-            .status(200)
-            .appendHeader("Content-Type", "application/oauth-authz-req+jwt")
-            .send(token);
-        }
-      } else {
-        res.status(500).end();
-        return;
-      }
-    } else if (method === "POST") {
-      console.log("LOGIN API POST BY ID");
-
-      // Parse the JSON string into a JavaScript object
-      const presentation = JSON.parse(req.body.vp_token);
-      console.log("Presentation: \n", req.body.vp_token);
-
-      const uuid = presentation["proof"]["challenge"];
-      const policy = await redis.get(uuid + "_policy");
-
-      if (policy) {
-        const policyObject = JSON.parse(policy) as LoginPolicy;
-
-        // Constants for Redis to store the authentication result
-        const MAX_AGE = 20 * 60;
-        const EXPIRY_MS = "EX";
-
-        // Verify the presentation and the status of the credential
-        if (await verifyAuthenticationPresentation(presentation)) {
-          console.log("Presentation valid");
-          // Evaluate if the VP should be trusted
-          if (isTrustedPresentation(presentation, policyObject)) {
-            console.log("Presentation verified");
-
-            // Get the user claims when the presentation is trusted
-            const userClaims = extractClaims(presentation, policyObject);
-            console.log(userClaims);
-
-            // Store the authentication result in Redis
-            await redis.set(uuid + "_auth-res", "success", EXPIRY_MS, MAX_AGE);
-
-            // Store the user claims in Redis
-            await redis.set(
-              uuid + "_claims",
-              JSON.stringify(userClaims.tokenId),
-              EXPIRY_MS,
-              MAX_AGE,
-            );
-          } else {
-            console.log("Presentation not trusted");
-
-            await redis.set(
-              "auth_res:" + uuid,
-              "error_presentation_not_trused",
-              EXPIRY_MS,
-              MAX_AGE,
-            );
-            // Wallet gets an error message
-            res.status(500).end();
-            return;
-          }
-        } else {
-          console.log("Presentation invalid");
-          await redis.set(
-            "auth_res:" + uuid,
-            "error_invalid_presentation",
-            EXPIRY_MS,
-            MAX_AGE,
-          );
-          res.status(500).end();
-          return;
-        }
-
-        // Wallet gets 200 status code
-        res.status(200).end();
-      }
+    if (method) {
+      const execute = handlers[method.toUpperCase()];
+      return execute(req, res);
     }
   } catch (error) {
     res.status(500).end();
--- vclogin/pages/api/presentCredential.ts
+++ vclogin/pages/api/presentCredential.ts
@@ -21,118 +21,128 @@ try {
   console.error("Failed to connect to Redis:", error);
 }
 
+const getHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  console.log("LOGIN API GET");
+  console.log(req.query);
+  const presentation_definition = generatePresentationDefinition(
+    getConfiguredLoginPolicy()!,
+  );
+
+  const challenge = req.query["login_id"];
+
+  if (challenge) {
+    const token = await getToken(
+      challenge as string,
+      process.env.EXTERNAL_URL + "/api/clientMetadata",
+      process.env.EXTERNAL_URL + "/api/presentCredential",
+      presentation_definition,
+      res,
+    );
+    console.log("Token: \n", token);
+
+    res
+      .status(200)
+      .appendHeader("Content-Type", "application/oauth-authz-req+jwt")
+      .send(token);
+  }
+};
+
+const postHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  console.log("LOGIN API POST");
+
+  // Parse the JSON string into a JavaScript object
+  const presentation = JSON.parse(req.body.vp_token);
+  console.log("Presentation: \n", req.body.vp_token);
+
+  // Verify the presentation and the status of the credential
+  if (await verifyAuthenticationPresentation(presentation)) {
+    // Evaluate if the VP should be trusted
+    if (isTrustedPresentation(presentation)) {
+      console.log("Presentation verified");
+    } else {
+      console.log("Presentation not trusted");
+      res.status(500).end();
+      return;
+    }
+  } else {
+    console.log("Presentation invalid");
+    res.status(500).end();
+    return;
+  }
+
+  // Get the user claims
+  const userClaims = extractClaims(presentation);
+  const subject = presentation["holder"];
+  const login_id = presentation["proof"]["challenge"];
+  const challenge = (await redis.get("" + login_id))!;
+  console.log("Logging in: " + subject + " with challenge: " + challenge);
+
+  // hydra login
+  await hydraAdmin
+    .adminGetOAuth2LoginRequest(challenge)
+    .then(({ data: loginRequest }) =>
+      hydraAdmin
+        .adminAcceptOAuth2LoginRequest(challenge, {
+          // Subject is an alias for user ID. A subject can be a random string, a UUID, an email address, ....
+          subject,
+          // This tells hydra to remember the browser and automatically authenticate the user in future requests. This will
+          // set the "skip" parameter in the other route to true on subsequent requests!
+          remember: Boolean(false),
+          // When the session expires, in seconds. Set this to 0 so it will never expire.
+          remember_for: 3600,
+          // Sets which "level" (e.g. 2-factor authentication) of authentication the user has. The value is really arbitrary
+          // and optional. In the context of OpenID Connect, a value of 0 indicates the lowest authorization level.
+          // acr: '0',
+          //
+          // If the environment variable CONFORMITY_FAKE_CLAIMS is set we are assuming that
+          // the app is built for the automated OpenID Connect Conformity Test Suite. You
+          // can peak inside the code for some ideas, but be aware that all data is fake
+          // and this only exists to fake a login system which works in accordance to OpenID Connect.
+          //
+          // If that variable is not set, the ACR value will be set to the default passed here ('0')
+          acr: "0",
+        })
+        .then(({ data: body }) => {
+          const MAX_AGE = 30; // 30 seconds
+          const EXPIRY_MS = "EX"; // seconds
+
+          // save the user claims to redis
+          redis.set(
+            "" + subject,
+            JSON.stringify(userClaims),
+            EXPIRY_MS,
+            MAX_AGE,
+          );
+
+          // save the redirect address to redis for the browser
+          redis.set(
+            "redirect" + login_id,
+            String(body.redirect_to),
+            EXPIRY_MS,
+            MAX_AGE,
+          );
+          // phone just gets a 200 ok
+          res.status(200).end();
+        }),
+    )
+    // This will handle any error that happens when making HTTP calls to hydra
+    .catch((_) => res.status(401).end());
+};
+
+const handlers: any = {
+  GET: getHandler,
+  POST: postHandler,
+};
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>,
 ) {
   try {
     const { method } = req;
-    if (method === "GET") {
-      console.log("LOGIN API GET");
-      console.log(req.query);
-      const presentation_definition = generatePresentationDefinition(
-        getConfiguredLoginPolicy()!,
-      );
-
-      const challenge = req.query["login_id"];
-
-      if (challenge) {
-        const token = await getToken(
-          challenge as string,
-          process.env.EXTERNAL_URL + "/api/clientMetadata",
-          process.env.EXTERNAL_URL + "/api/presentCredential",
-          presentation_definition,
-          res,
-        );
-        console.log("Token: \n", token);
-
-        res
-          .status(200)
-          .appendHeader("Content-Type", "application/oauth-authz-req+jwt")
-          .send(token);
-      }
-    } else if (method === "POST") {
-      console.log("LOGIN API POST");
-
-      // Parse the JSON string into a JavaScript object
-      const presentation = JSON.parse(req.body.vp_token);
-      console.log("Presentation: \n", req.body.vp_token);
-
-      // Verify the presentation and the status of the credential
-      if (await verifyAuthenticationPresentation(presentation)) {
-        // Evaluate if the VP should be trusted
-        if (isTrustedPresentation(presentation)) {
-          console.log("Presentation verified");
-        } else {
-          console.log("Presentation not trusted");
-          res.status(500).end();
-          return;
-        }
-      } else {
-        console.log("Presentation invalid");
-        res.status(500).end();
-        return;
-      }
-
-      // Get the user claims
-      const userClaims = extractClaims(presentation);
-      const subject = presentation["holder"];
-      const login_id = presentation["proof"]["challenge"];
-      const challenge = (await redis.get("" + login_id))!;
-      console.log("Logging in: " + subject + " with challenge: " + challenge);
-
-      // hydra login
-      await hydraAdmin
-        .adminGetOAuth2LoginRequest(challenge)
-        .then(({ data: loginRequest }) =>
-          hydraAdmin
-            .adminAcceptOAuth2LoginRequest(challenge, {
-              // Subject is an alias for user ID. A subject can be a random string, a UUID, an email address, ....
-              subject,
-              // This tells hydra to remember the browser and automatically authenticate the user in future requests. This will
-              // set the "skip" parameter in the other route to true on subsequent requests!
-              remember: Boolean(false),
-              // When the session expires, in seconds. Set this to 0 so it will never expire.
-              remember_for: 3600,
-              // Sets which "level" (e.g. 2-factor authentication) of authentication the user has. The value is really arbitrary
-              // and optional. In the context of OpenID Connect, a value of 0 indicates the lowest authorization level.
-              // acr: '0',
-              //
-              // If the environment variable CONFORMITY_FAKE_CLAIMS is set we are assuming that
-              // the app is built for the automated OpenID Connect Conformity Test Suite. You
-              // can peak inside the code for some ideas, but be aware that all data is fake
-              // and this only exists to fake a login system which works in accordance to OpenID Connect.
-              //
-              // If that variable is not set, the ACR value will be set to the default passed here ('0')
-              acr: "0",
-            })
-            .then(({ data: body }) => {
-              const MAX_AGE = 30; // 30 seconds
-              const EXPIRY_MS = "EX"; // seconds
-
-              // save the user claims to redis
-              redis.set(
-                "" + subject,
-                JSON.stringify(userClaims),
-                EXPIRY_MS,
-                MAX_AGE,
-              );
-
-              // save the redirect address to redis for the browser
-              redis.set(
-                "redirect" + login_id,
-                String(body.redirect_to),
-                EXPIRY_MS,
-                MAX_AGE,
-              );
-              // phone just gets a 200 ok
-              res.status(200).end();
-            }),
-        )
-        // This will handle any error that happens when making HTTP calls to hydra
-        .catch((_) => res.status(401).end());
-    } else {
-      res.status(500).end();
+    if (method) {
+      const execute = handlers[method.toUpperCase()];
+      return execute(req, res);
     }
   } catch (e) {
     res.status(500).end();
