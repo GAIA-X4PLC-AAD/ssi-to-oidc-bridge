a68b79a06cf286f11d2476eeffc91810cbf0d17b
Felix Hoops <9974641+jfelixh@users.noreply.github.com>
2024-06-12 15:42:06 +0200

    Troubleshooting playwright
    Signed-off-by: Felix Hoops <9974641+jfelixh@users.noreply.github.com>

--- vclogin/__tests__/e2e/e2e.spec.ts
+++ vclogin/__tests__/e2e/e2e.spec.ts
@@ -41,7 +41,7 @@ test.describe("Index Page", () => {
 
     await expect(
       page.getByRole("heading", { name: "SSI-to-OIDC Bridge" }),
-    ).toBeVisible();
+    ).toBeVisible({ timeout: 60000 });
   });
 });
 
@@ -51,20 +51,20 @@ test.describe("Login Page", () => {
 
     await expect(
       page.getByRole("heading", { name: "SSI-to-OIDC Bridge" }),
-    ).toBeVisible({ timeout: 15000 });
+    ).toBeVisible({ timeout: 60000 });
   });
 
   test("has CTA", async ({ page }) => {
     await page.goto(url + "/login?login_challenge=challenge123");
 
     await expect(page.getByRole("heading", { name: /Scan/i })).toBeVisible({
-      timeout: 15000,
+      timeout: 60000,
     });
   });
 
   test("has QR Code", async ({ page }) => {
     await page.goto(url + "/login?login_challenge=challenge123");
 
-    await expect(page.locator("canvas")).toBeVisible({ timeout: 15000 });
+    await expect(page.locator("canvas")).toBeVisible({ timeout: 60000 });
   });
 });
