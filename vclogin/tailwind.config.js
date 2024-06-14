057951071eb5e26c6a133d604e53b3053305a559
Felix Hoops <9974641+jfelixh@users.noreply.github.com>
2024-06-14 13:26:01 +0200

    Design overhaul
    Signed-off-by: Felix Hoops <9974641+jfelixh@users.noreply.github.com>

--- vclogin/tailwind.config.js
+++ vclogin/tailwind.config.js
@@ -11,16 +11,11 @@ module.exports = withMT({
     "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      colors: {
-        nearBlack: "#111111",
-      },
+    colors: {
+      gxblue: "#000094",
+      gxpurple: "#B900FF",
     },
+    extend: {},
   },
   plugins: [],
 });
