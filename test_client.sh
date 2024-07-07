d79b424e83a96a32c936557afedbddb8781b6c42
Ilayda Cansin Koc <ilaydacansin@gmail.com>
2024-07-07 20:18:44 +0000

    update test_client.sh to use next auth redirect
    Signed-off-by: Ilayda Cansin Koc <ilaydacansin@gmail.com>

--- test_client.sh
+++ test_client.sh
@@ -8,13 +8,11 @@ client=$(docker run --rm -it \
     --grant-type authorization_code \
     --response-type token,code,id_token \
     --scope openid \
-    --redirect-uri http://localhost:9010/callback \
+    --redirect-uri "http://localhost:3000/api/auth/callback/oidc" \
     -e http://hydra:4445 \
     --format json )
 
 echo $client
-
-client_id=$(echo $client | jq -r '.client_id')
 
 docker run --rm -it \
     --network ory-hydra-net \
@@ -24,8 +22,8 @@ docker run --rm -it \
     --port 9010 \
     --client-id $client_id \
     --client-secret some-secret \
-    --redirect http://localhost:9010/callback \
+    --redirect "http://localhost:3000/api/auth/callback/oidc" \
     --scope openid \
     --auth-url http://localhost:5004/oauth2/auth \
-    --token-url http://hydra:4444/oauth2/token \
+    --token-url http://localhost:5004/oauth2/token \
     -e http://hydra:4444
